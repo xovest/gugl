5678fa91772f4284467c2cab5f9776014f1ea622
xovest <79008917+xovest@users.noreply.github.com>
2021-12-08 21:45:50 +0200

    router changes up to date

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
-import { Routes, Route, Redirect } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import { Results } from './Results';
 
 export const RoutesComp = () => (
   <div className="p-4">
     <Routes>
-      <Route exact path="/">
-        <Redirect to="/search" />
-      </Route>
-      <Route exact path="/search">
-        <Results />
-      </Route>
-      <Route path="/images">
-        <Results />
-      </Route>
-      <Route path="/news">
-        <Results />
-      </Route>
-      <Route path="/videos">
-        <Results />
-      </Route>
-
+      <Route exact path="/" element={<Results />} />
+      <Route exact path="/search" element={<Results />} />
+      <Route path="/images" element={<Results />} />
+      <Route path="/news" element={<Results />} />
+      <Route path="/videos" element={<Results />} />
     </Routes>
   </div>
 
